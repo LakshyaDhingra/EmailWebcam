42e4fcbaeb85faa4e8c449dab0026a5c235d5125
Lakshya Dhingra <dlakshya223@gmail.com>
2024-12-28 15:43:59 +0530

    Webcam taking the best image from when object comes and goes and stores it

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
@@ -29,12 +32,18 @@ while True:
     contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 30000:
+        if cv2.contourArea(contour) < 20000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            # Producing images when object comes and goes
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            middle_image_index = int(len(all_images)/2)
+            middle_image = all_images[middle_image_index]
 
     status_list.append(status)
     status_list = status_list[-2:]
